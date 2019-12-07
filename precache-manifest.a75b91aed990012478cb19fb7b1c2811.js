8616969633523137480cf789e55472da16981460
Shagufta <shagufta.khan@intelliswift.co.in>
2019-12-07 18:57:15 +0530

    Updates

---
+++ precache-manifest.a75b91aed990012478cb19fb7b1c2811.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40e4e47df27d8b7ec31b3da2dceef425",
+    "url": "/Monsters-Rolodex/index.html"
+  },
+  {
+    "revision": "7d4f2dd41f69bd865ea8",
+    "url": "/Monsters-Rolodex/static/css/main.a7972165.chunk.css"
+  },
+  {
+    "revision": "4347ccf15f8757139bac",
+    "url": "/Monsters-Rolodex/static/js/2.0096a813.chunk.js"
+  },
+  {
+    "revision": "7d4f2dd41f69bd865ea8",
+    "url": "/Monsters-Rolodex/static/js/main.bb36bcbd.chunk.js"
+  },
+  {
+    "revision": "b7467f4eccddd9e00be3",
+    "url": "/Monsters-Rolodex/static/js/runtime-main.0a405321.js"
+  }
+]);
